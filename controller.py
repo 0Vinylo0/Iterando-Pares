d16450fc360154f79830eaff5588766a9c8cda6b
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2025-05-08 15:09:13 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,7 +1,18 @@
+# controller.py
 from iterando import initialize_redis, run_scraper
 from multiprocessing import Process
+import argparse
 
 if __name__ == "__main__":
+    # Parse command-line option to skip downloads
+    parser = argparse.ArgumentParser(description='Iniciar scraper con opción de omitir descarga de imágenes')
+    parser.add_argument(
+        '--skip-download',
+        action='store_true',
+        help='Si se especifica, se omite la descarga de imágenes, pero se generan los enlaces en description.json'
+    )
+    args = parser.parse_args()
+
     # Inicializa Redis con las URLs desde el archivo externo
     initialize_redis("todo_urls.txt")
 
@@ -10,7 +21,8 @@ if __name__ == "__main__":
     processes = []
 
     for _ in range(num_processes):
-        p = Process(target=run_scraper)
+        # Pasar el flag skip_download a run_scraper
+        p = Process(target=run_scraper, args=(args.skip_download,))
         p.start()
         processes.append(p)
 
