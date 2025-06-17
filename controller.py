456af96447fbb15cd7a48ac9a2ae0c465fc0243d
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2025-06-17 11:55:52 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,15 +1,21 @@
-# controller.py
 from iterando import initialize_redis, run_scraper
 from multiprocessing import Process
 import argparse
 
 if __name__ == "__main__":
     # Parse command-line option to skip downloads
-    parser = argparse.ArgumentParser(description='Iniciar scraper con opción de omitir descarga de imágenes')
+    parser = argparse.ArgumentParser(
+        description='Iniciar scraper con opción de omitir descarga de imágenes'
+    )
     parser.add_argument(
         '--skip-download',
         action='store_true',
         help='Si se especifica, se omite la descarga de imágenes, pero se generan los enlaces en description.json'
+    )
+    parser.add_argument(
+        '--tor-disable',
+        action='store_true',
+        help='Desactiva el uso de Tor (no proxy)'
     )
     args = parser.parse_args()
 
@@ -21,8 +27,14 @@ if __name__ == "__main__":
     processes = []
 
     for _ in range(num_processes):
-        # Pasar el flag skip_download a run_scraper
-        p = Process(target=run_scraper, args=(args.skip_download,))
+        # Pasar los flags skip_download y tor_disable a run_scraper
+        p = Process(
+            target=run_scraper,
+            kwargs={
+                'skip_download': args.skip_download,
+                'tor_disable': args.tor_disable
+            }
+        )
         p.start()
         processes.append(p)
 
